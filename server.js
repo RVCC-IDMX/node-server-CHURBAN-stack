5166f65d6e0c86eeb333a24af826826e5f1a4835
CHURBAN-stack <chur6789@Stu.raritanval.edu>
2021-10-27 12:42:53 -0400

    Do status codes

--- server.js
+++ server.js
@@ -4,18 +4,20 @@ const fs = require('fs');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   
   let path = './';
   switch(req.url) {
   case '/':
+  res.statusCode = 200;
   break; 
   case '/about': 
   path += 'about.html';
+  res.statusCode = 200;
   break; 
   default:
   path += '404.html';
+  res.statusCode = 404;
   break; 
   }
   
